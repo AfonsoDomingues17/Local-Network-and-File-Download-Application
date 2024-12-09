d637d2a852c749352c64bc7463217432b8f11c71
DuarteSAssuncao <up202208319@up.pt>
2024-12-09 14:07:34 +0000

    Until half of exp5
    Former-commit-id: b1013e6f368ccf18abbcc2e11dea6127f1edd4c9

--- config-tux33.sh
+++ config-tux33.sh
@@ -2,34 +2,40 @@ systemctl restart networking
 
 # Exp1:
 ifconfig eth1 up
-ifconfig eth1 172.16.30.1/24
+ifconfig eth1 172.16.50.1/24
 # ifconfig # Register IP and MAC addresses
 # route -n # Inspect Forwarding
 # arp -a # Inspect ARP tables
-# arp -d 172.16.30.254 # Delete entry for TUX34 (not mandatory for configuration)
+# arp -d 172.16.50.254 # Delete entry for TUX54 (not mandatory for configuration)
 # Start Wrireshark on eth1
-# ping 172.16.30.254 # Ping TUX34
+# ping 172.16.50.254 # Ping TUX54
 
 # Exp2:
 # Start Wrireshark on eth1
-# ping 172.16.30.254
-# ping 172.16.31.1
-# Start Wrireshark on eth1. Also start it on eth1 of TUX32 and TUX34
-# ping -b 172.16.30.255 # Ping broadcast
+# ping 172.16.50.254
+# ping 172.16.51.1
+# Start Wrireshark on eth1. Also start it on eth1 of TUX52 and TUX54
+# ping -b 172.16.50.255 # Ping broadcast
 
 # Exp3:
-route add -net 172.16.31.0/24 gw 172.16.30.254
+route add -net 172.16.51.0/24 gw 172.16.50.254
 # route -n # Observe routes
 # Start Wrireshark on eth1.
-# ping 172.16.30.254 # Ping and verify connectivity
-# ping 172.16.31.253 # Ping and verify connectivity
-# ping 172.16.31.1 # Ping and verify connectivity
-# Start Wrireshark on eth1 AND eth2 of TUX34.
-# ping 172.16.31.1 # Ping and sabe logs of TUX34
-# arp -d 172.16.30.254 # Delete entry for TUX34 (not mandatory for configuration)
+# ping 172.16.50.254 # Ping and verify connectivity
+# ping 172.16.51.253 # Ping and verify connectivity
+# ping 172.16.51.1 # Ping and verify connectivity
+# Start Wrireshark on eth1 AND eth2 of TUX54.
+# ping 172.16.51.1 # Ping and sabe logs of TUX54
+# arp -d 172.16.50.254 # Delete entry for TUX54 (not mandatory for configuration)
 
 # Exp4:
-# route -n # Verify the default route (0.0.0.0) is TUX34
-# If there is an existing default router, delete it: route del ...
-route add default gw 172.16.30.254
+route add -net 172.16.1.0/24 gw 172.16.50.254
+
 # TODO
+
+
+
+# Exp5:
+echo domain netlab.fe.up.pt > /etc/resolv.conf
+echo search netlab.fe.up.pt >> /etc/resolv.conf
+echo nameserver 10.227.20.3 >> /etc/resolv.conf
--- config-tux34.sh
+++ config-tux34.sh
@@ -2,24 +2,28 @@ systemctl restart networking
 
 # Exp1:
 ifconfig eth1 up
-ifconfig eth1 172.16.30.254/24
+ifconfig eth1 172.16.50.254/24
 # ifconfig # Register IP and MAC addresses
 # route -n # Inspect Forwarding
 # arp -a # Inspect ARP tables
 
 # Exp3:
 ifconfig eth2 up
-ifconfig eth2 172.16.31.253/24
+ifconfig eth2 172.16.51.253/24
 sysctl net.ipv4.ip_forward=1
 sysctl net.ipv4.icmp_echo_ignore_broadcasts=0
 # ifconfig # Register IP and MAC addresses of eth1 and eth2
 # route -n # Observe routes
-# Start Wrireshark on eth1 AND eth2. On TUX33, ping TUX32
-# arp -d 172.16.31.1 # Delete entry for TUX32 (not mandatory for configuration)
-# arp -d 172.16.30.1 # Delete entry for TUX33 (not mandatory for configuration)
+# Start Wrireshark on eth1 AND eth2. On TUX53, ping TUX52
+# arp -d 172.16.51.1 # Delete entry for TUX52 (not mandatory for configuration)
+# arp -d 172.16.50.1 # Delete entry for TUX53 (not mandatory for configuration)
 
 # Exp4:
-# route -n # Verify the default route (0.0.0.0) is ROUTER (172.16.31.254)
-# If there is an existing default router, delete it: route del ...
-route add default gw 172.16.31.254
+route add -net 172.16.1.0/24 gw 172.16.51.254
 # TODO
+
+
+# Exp5:
+echo domain netlab.fe.up.pt > /etc/resolv.conf
+echo search netlab.fe.up.pt >> /etc/resolv.conf
+echo nameserver 10.227.20.3 >> /etc/resolv.conf
